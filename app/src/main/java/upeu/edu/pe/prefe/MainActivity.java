3124d2fb30afb030223fc884c68c9721b237e586
Alumno FIA - L8 <alum.fial8@lim.upeu.edu.pe>
2018-10-09 16:40:51 -0500

    Initial commit

---
+++ app/src/main/java/upeu/edu/pe/prefe/MainActivity.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.prefe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String correo = "bdreyna2018@gmail.com";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
